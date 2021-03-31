42d6ad0b64bec4ddddd53fc91874662848b7d1aa
ilmoroz <ilmoroz@outlook.com>
2021-03-31 19:38:54 +0300

    first commit

---
+++ src/components/Team-List.vue
@@ -0,0 +1,40 @@
+<div class="col-lg-2" style="display: grid; align-content: space-between;">
+                       <div class="team-block">
+                           <div class="team-img">
+                               <img src="../assets/img/teams/bogurov.jpg" alt="">
+                           </div>
+                           <div class="team-name">
+                                Борис
+                                Богуров
+                           </div>
+                           <div class="team-pos">
+                                Пресс-секретарь
+                           </div>
+                       </div>
+                       <div class="team-block">
+                           <div class="team-img">
+                               <img src="../assets/img/teams/bogurov.jpg" alt="">
+                           </div>
+                           <div class="team-name">
+                                Борис
+                                Богуров
+                           </div>
+                           <div class="team-pos">
+                                Пресс-секретарь
+                           </div>
+                       </div>
+                   </div>
+                    <div class="col-lg-2" style="display: grid; align-content: center;">
+                       <div class="team-block">
+                           <div class="team-img">
+                               <img src="../assets/img/teams/bogurov.jpg" alt="">
+                           </div>
+                           <div class="team-name">
+                                Борис
+                                Богуров
+                           </div>
+                           <div class="team-pos">
+                                Пресс-секретарь
+                           </div>
+                       </div>
+                   </div>
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,108 +1,98 @@
 <template>
     <div id="layout-teams" class="section">
-       <div class="layout-menu-teams">
-           <div class="container-fluid">
-               <div class="row justify-content-center">
-                   <div class="col-lg-4">
-                       <div class="menu-name">
-                           Команда <br> нашего <br> проекта
-                       </div>
-                      <div class="menu-select-dep">
-                          Выберите <br> отдел
-                      </div>
-                      <div class="menu-teams">
-                          <ul>
-                              <li class="sel-active"><a href="">Ц</a></li>
-                              <li><a href="">ЦА ( Разраб. )</a></li>
-                              <li><a href="">ЦД</a></li>
-                              <li><a href="">ЦРБ</a></li>
-                              <li><a href="">ЦТ</a></li>
-                              <li><a href="">Метро</a></li>
-                          </ul>
-                      </div>
-                   </div>
-                   <div class="col-lg-2" style="display: grid; align-content: space-between; ">
-                       <div class="team-block">
-                           <div class="team-img">
-                               <img src="../assets/img/teams/bogurov.jpg" alt="">
-                           </div>
-                           <div class="team-name">
-                                Борис
-                                Богуров
-                           </div>
-                           <div class="team-pos">
-                                Пресс-секретарь
-                           </div>
-                       </div>
-                       <div class="team-block">
-                           <div class="team-img">
-                               <img src="../assets/img/teams/bogurov.jpg" alt="">
-                           </div>
-                           <div class="team-name">
-                                Борис
-                                Богуров
-                           </div>
-                           <div class="team-pos">
-                                Пресс-секретарь
-                           </div>
-                       </div>
-                   </div>
-                    <div class="col-lg-2" style="display: grid; align-content: center;">
-                       <div class="team-block">
-                           <div class="team-img">
-                               <img src="../assets/img/teams/bogurov.jpg" alt="">
-                           </div>
-                           <div class="team-name">
-                                Борис
-                                Богуров
-                           </div>
-                           <div class="team-pos">
-                                Пресс-секретарь
-                           </div>
-                       </div>
-                   </div>
-                    <div class="col-lg-2" style="display: grid; align-content: space-between; ">
-                      <div class="team-block">
-                           <div class="team-img">
-                               <img src="../assets/img/teams/bogurov.jpg" alt="">
-                           </div>
-                           <div class="team-name">
-                                Борис
-                                Богуров
-                           </div>
-                           <div class="team-pos">
-                                Пресс-секретарь
-                           </div>
-                       </div>
-                      <div class="team-block">
-                           <div class="team-img">
-                               <img src="../assets/img/teams/bogurov.jpg" alt="">
-                           </div>
-                           <div class="team-name">
-                                Борис
-                                Богуров
-                           </div>
-                           <div class="team-pos">
-                                Пресс-секретарь
-                           </div>
-                       </div>
-                   </div>
-                   <div class="col-lg-2" style="display: grid; align-content: center;">
-                       <div class="team-block">
-                           <div class="team-img">
-                               <img src="../assets/img/teams/bogurov.jpg" alt="">
-                           </div>
-                           <div class="team-name">
-                                Борис
-                                Богуров
-                           </div>
-                           <div class="team-pos">
-                                Пресс-секретарь
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4 offset-1">
+                    <div class="menu-teams-block">
+                        <div class="menu-name">
+                            Команда <br> нашего <br> проекта
+                        </div>
+                        <div class="menu-select-dep">
+                            Выберите <br> отдел
+                        </div>
+                        <div class="menu-teams">
+                            <ul>
+                                <li class="sel-active"><a href="">Ц</a></li>
+                                <li><a href="">ЦА ( Разраб. )</a></li>
+                                <li><a href="">ЦД</a></li>
+                                <li><a href="">ЦРБ</a></li>
+                                <li><a href="">ЦТ</a></li>
+                                <li><a href="">Метро</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="row" style="height:80vh; justify-content: space-between;">
+                        <div class="col-lg-4">
+                            <div class="team-img">
+                                <img src="../assets/img/teams/bogurov.jpg" alt="">
+                            </div>
+                            <div class="team-name">
+                                Богуренок
+                            </div>
+                            <div class="team-pos">
+                                Начальник
+                            </div>
+                        </div>
+                         <div class="col-lg-4">
+                            <div class="team-img">
+                                <img src="../assets/img/teams/bogurov.jpg" alt="">
+                            </div>
+                            <div class="team-name">
+                                Богуренок
+                            </div>
+                            <div class="team-pos">
+                                Начальник
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="team-img">
+                                <img src="../assets/img/teams/bogurov.jpg" alt="">
+                            </div>
+                            <div class="team-name">
+                                Богуренок
+                            </div>
+                            <div class="team-pos">
+                                Начальник
+                            </div>
+                        </div>
+                         <div class="col-lg-4">
+                            <div class="team-img">
+                                <img src="../assets/img/teams/bogurov.jpg" alt="">
+                            </div>
+                            <div class="team-name">
+                                Богуренок
+                            </div>
+                            <div class="team-pos">
+                                Начальник
+                            </div>
+                        </div>
+                         <div class="col-lg-4">
+                            <div class="team-img">
+                                <img src="../assets/img/teams/bogurov.jpg" alt="">
+                            </div>
+                            <div class="team-name">
+                                Богуренок
+                            </div>
+                            <div class="team-pos">
+                                Начальник
+                            </div>
+                        </div>
+                         <div class="col-lg-4">
+                            <div class="team-img">
+                                <img src="../assets/img/teams/bogurov.jpg" alt="">
+                            </div>
+                            <div class="team-name">
+                                Богуренок
+                            </div>
+                            <div class="team-pos">
+                                Начальник
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
